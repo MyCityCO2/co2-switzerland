d15d939a2b531ab5bbde49d9bf3fbd27501695ec
Haefelin Julien (IT-PTR-CEN1-YPT2) <julien.haefelin@sbb.ch>
2024-03-15 11:26:50 +0100

    add scripts to join geodata on GDENR and KTNR in order ot create city uid add scripts to join city uid to populations try to debug check_data - UNSUCCESSFULL

--- check_jura_data.py
+++ check_jura_data.py
@@ -3,14 +3,20 @@ import os
 import warnings
 import numpy as np
 
+datafolder = 'data'
+geo_filename = 'BFS_Geodata_concat.csv'
+
+geo_df = pd.read_csv(os.path.join(datafolder, geo_filename))
+city_to_uid = geo_df[['GDENAME', 'UID']].set_index('GDENAME').squeeze().to_dict()
+
 year = 2022
 filename = '4.2.-Compte-de-resultats-par-commune.'+str(year)
-index_header = 2
+index_header = 3
+index_uid_label = 1
 
 test_filename = filename+'_clean.csv'
 truth_filename = filename+'.xlsx'
 
-datafolder = 'data'
 test_path = os.path.join(datafolder, test_filename)
 truth_path = os.path.join(datafolder, truth_filename)
 
@@ -22,20 +28,22 @@ mask = truth_df.iloc[:,1].isnull()
 truth_df= truth_df[~mask].dropna(axis=1).drop(columns = 'Unnamed: 3').rename(columns = {'Unnamed: 1' : 'category'})
 truth_df['category'] = truth_df['category'].astype(int)
 truth_df.set_index('category', inplace=True)
+truth_df =truth_df.drop(columns= [c for c in truth_df.columns if pd.isnull(c) or c not in list(city_to_uid.keys())]) # drop every other columns as they unknown
+truth_df.columns = [city_to_uid[c] for c in truth_df.columns]
 
-# clean test df to keep only sums pro categories
-test_df['category'] = test_df['account'].astype(str).str[:2].astype(int)
-test_df = test_df.groupby(['category', 'uid_city']).agg({'result' : 'sum'}).unstack()
-test_df.columns = test_df.columns.droplevel()
+# # # clean test df to keep only sums pro categories
+# test_df['category'] = test_df['account'].astype(str).str[:2].astype(int)
+# test_df = test_df.groupby(['category', 'identifier']).agg({'result' : 'sum'}).unstack()
+# test_df.columns = test_df.columns.droplevel()
 
-categories = list(test_df.index)
-cities_uid = list(test_df.columns)
-bol = False
+# categories = list(test_df.index)
+# cities_uid = list(test_df.columns)
 
-for cat in categories : 
-    for uid in cities_uid : 
-        test_value = np.round(test_df.loc[cat, uid], 3)
-        truth_value = np.round(truth_df.loc[cat, uid], 3)
+# for cat in categories : 
+#     for uid in cities_uid : 
+#         test_value = np.round(test_df.loc[cat, uid], 3)
+#         truth_value = np.round(truth_df.loc[cat, uid], 3)
 
-        if test_value != truth_value : 
-            warnings.warn("Uid "+str(uid) + " of value "+str(test_value)+" does not correspond to the truth value of "+str(truth_value))
+
+#         if test_value != truth_value : 
+#             warnings.warn("Uid "+str(uid) + " of value "+str(test_value)+" does not correspond to the truth value of "+str(truth_value)+ ' for category '+str(cat))
--- clean_jura_data.py
+++ clean_jura_data.py
@@ -10,33 +10,36 @@ year = 2022
 filetype = 'xlsx'
 filename = '4.2.-Compte-de-resultats-par-commune.'+str(year)
 datafolder = 'data'
+geo_filename = 'BFS_Geodata_concat.csv'
 path = os.path.join(datafolder, filename+'.'+filetype)
 
 df = pd.read_excel(path, header=index_header, sheet_name="4.1 Comptes "+str(year)+" natures").dropna(axis = 0, how = 'all').dropna(axis = 1, how = 'all') # clean df from empty lines/columns
+geo_df = pd.read_csv(os.path.join(datafolder, geo_filename))
 
-uid_to_cities_map = { k: v  for k,v in df.iloc[0].to_dict().items() if isinstance(k, int)} # keep city name and corresponding uid
-df.drop(index = 0, inplace = True) # drop city name as they are saved above. TODO: save it as json ?
+city_to_uid = geo_df[['GDENAME', 'UID']].set_index('GDENAME').squeeze().to_dict()
 
-col_uid_label = list(df.columns)[index_uid_label] # retrieve the column uid-for-labels from defined index above
-mask = df[col_uid_label].isna() # use it as mask as everything else is unnecessary lines
+cities_to_pop = { v: k  for k,v in df.iloc[0].to_dict().items() if isinstance(k, int)} # keep city name and corresponding population
+df.columns = df.iloc[0]
+df.drop(index = 0, inplace = True) # drop city name as they are used as column name
+
+mask = df.iloc[:,index_uid_label].isna() # use it as mask as everything else is unnecessary lines
 df = df[~mask].dropna(axis=1, how='any')
 
-df.index = df[col_uid_label] #set label uid as index
+df.index = df.iloc[:,0] #set label uid as index
 df = df.rename_axis('uid', axis='index')
 
-uid_to_label_map = { k: v  for k,v in df.iloc[0].to_dict().items() if isinstance(k, int)} # keep label name and corresponding uid
-df =df.drop(columns= [c for c in df.columns if not isinstance(c, int)]) # drop every other columns as they unknown
-
+df =df.drop(columns= [c for c in df.columns if pd.isnull(c) or c not in list(city_to_uid.keys())]) # drop every other columns as they unknown
 df = df.loc[[uid for uid in df.index if len(str(uid)) == 3]] # keep only valid uid for labels
+df.columns.name = 'city_name'
 
 # pivot data and add other informations and save it
 cleaned = df.stack().reset_index()
-cleaned.columns = ['account', 'uid_city', 'result']
-cleaned['city'] = cleaned['uid_city'].map(uid_to_cities_map)
+cleaned.columns = ['account', 'city_name', 'result']
+cleaned['identifier'] = cleaned['city_name'].map(city_to_uid)
+cleaned['population'] = cleaned['city_name'].map(cities_to_pop)
 cleaned['currency'] = 'CHF'
 cleaned['chart_of_account'] = 'MCH2'
 cleaned['country_iso'] = 'CH'
 cleaned['date'] = datetime(year, 12, 31).strftime("%d.%m.%Y")
-cleaned['identifier'] = 'to_be_defined'
 
 cleaned.to_csv(os.path.join(datafolder, filename+'_clean.csv'), index=False)
